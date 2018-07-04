2b255f52f32f5e6a3284611eecb41f40701fcac0
DaveTilheim <mb624967@skynet.be>
2018-07-04 16:45:01 +0200

    add documentation

--- src/block.py
+++ src/block.py
@@ -7,20 +7,20 @@ class Block:
 	Block -> hitbox no visible
 	xbegin = xorigin of the block
 	ybegin = yorigin of the block
-	larg = width of the block
-	haut = height of the block
+	width = width of the block
+	height = height of the block
 	speed = the speed of the block if it moved
 	id = the id of the block
 	"""
 	nb_block = 0
 	list_block = []
-	def __init__(self, x=0, y=0, larg=100, haut=100, speed=5):
+	def __init__(self, x=0, y=0, width=100, height=100, speed=5):
 		self.xbegin = x
 		self.ybegin = y
-		self.larg = larg
-		self.haut = haut
-		self.xend = x+larg
-		self.yend = y+haut
+		self.width = width
+		self.height = height
+		self.xend = x+width
+		self.yend = y+height
 		self.speed = speed
 		self.id = Block.nb_block
 		self.time = None
@@ -30,8 +30,8 @@ class Block:
 	def set_position(self, x, y):
 		self.xbegin = x
 		self.ybegin = y
-		self.xend = x+self.larg
-		self.yend = y+self.haut
+		self.xend = x+self.width
+		self.yend = y+self.height
 
 	"""the block position has the same position as the mouse"""
 	def set_mouse_position(self, pos="center"):
@@ -39,7 +39,7 @@ class Block:
 		if pos is "origin":
 			self.set_position(mx, my)
 		elif pos is "center":
-			self.set_position(mx-self.larg//2, my-self.haut//2)
+			self.set_position(mx-self.width//2, my-self.height//2)
 	"""the block move with a direction and a speed"""
 	def move(self, direction, spd=None):
 		speed = self.speed
@@ -67,7 +67,7 @@ class Block:
 		del Block.list_block[self.id]
 		Block.nb_block -= 1
 
-	"""detect a collision with a block (larg=1, haut=1)"""
+	"""detect a collision with a block (width=1, height=1)"""
 	def cursor_collide(self, cursor):
 		if cursor.xbegin >= self.xbegin and cursor.xbegin <= self.xend and cursor.ybegin >= self.ybegin and cursor.ybegin <= self.yend:
 			return True
@@ -79,80 +79,74 @@ class Block:
 			return True
 		return False
 
-	"""activate a callback function"""
-	def event(self, callback, args=None):
-			if args == None:
-				callback()
-			else:
-				callback(args)
 	"""mult the size of the block by fact"""
 	def mult_size(self, fact=1):
 		if fact <= 0:
 			return 'error'
-		self.larg = int(self.larg * fact)
-		self.haut = int(self.haut * fact)
+		self.width = int(self.width * fact)
+		self.height = int(self.height * fact)
 		self.set_position(self.xbegin, self.ybegin)
 	"""divide the size of the block by fact"""
 	def div_size(self, fact=1):
 		if fact <= 0:
 			return 'error'
-		self.larg = self.larg//fact
-		self.haut = self.haut//fact
+		self.width = self.width//fact
+		self.height = self.height//fact
 		self.set_position(self.xbegin, self.ybegin)
 	"""pow the size of the block by fact"""
 	def pow_size(self, power=2):
 		if power < 0:
 			return 'error'
-		self.larg = self.larg**power
-		self.haut = self.haut**power
+		self.width = self.width**power
+		self.height = self.height**power
 		self.set_position(self.xbegin, self.ybegin)
 	"""mult the size of the block by fact with *="""
 	def __imul__(self, fact):
 		if fact <= 0:
 			return self
-		self.larg = int(self.larg * fact)
-		self.haut = int(self.haut * fact)
+		self.width = int(self.width * fact)
+		self.height = int(self.height * fact)
 		self.set_position(self.xbegin, self.ybegin)
 		return self
 	"""divide the size of the block by fact with /="""
 	def __itruediv__(self, fact):
 		if fact <= 0:
 			return self
-		self.larg = self.larg//fact
-		self.haut = self.haut//fact
+		self.width = self.width//fact
+		self.height = self.height//fact
 		self.set_position(self.xbegin, self.ybegin)
 		return self
 	"""divide the size of the block by fact with //="""
 	def __ifloordiv__(self, fact):
 		if fact <= 0:
 			return self
-		self.larg = self.larg//fact
-		self.haut = self.haut//fact
+		self.width = self.width//fact
+		self.height = self.height//fact
 		self.set_position(self.xbegin, self.ybegin)
 		return self
 	"""pow the size of the block by fact with **="""
 	def __ipow__(self, power):
 		if power < 0:
 			return self
-		self.larg = self.larg**power
-		self.haut = self.haut**power
+		self.width = self.width**power
+		self.height = self.height**power
 		self.set_position(self.xbegin, self.ybegin)
 		return self
 	"""add the size of the block with an other block with <<"""
 	def __lshift__(self, other):
-		self.larg += other.larg
-		self.haut += other.haut
+		self.width += other.width
+		self.height += other.height
 		self.set_position(self.xbegin, self.ybegin)
 	"""add the size of an other block with the instance with >>"""
 	def __rshift__(self, other):
 		other << self
 
 	def __str__(self):
-		return "{}x{} ({}x{}y)".format(self.larg, self.haut, self.xbegin, self.ybegin)
+		return "{}x{} ({}x{}y)".format(self.width, self.height, self.xbegin, self.ybegin)
 
 	def __repr__(self):
 		string = "pos: ({}, {})\ndim: ({}, {})\nspeed: {}\nid: #{}\n".format(
-			self.xbegin, self.ybegin, self.larg, self.haut, self.speed, self.id)
+			self.xbegin, self.ybegin, self.width, self.height, self.speed, self.id)
 		return string
 
 
@@ -165,8 +159,8 @@ class Drawblock(Block):
 	window = pygame.Surface()
 	fill = 0-> the block is filled with color
 	"""
-	def __init__(self, x=0, y=0, larg=100, haut=100, speed=5, color=(255,255,255), fill=0, window=None):
-		Block.__init__(self, x, y, larg, haut, speed)
+	def __init__(self, x=0, y=0, width=100, height=100, speed=5, color=(255,255,255), fill=0, window=None):
+		Block.__init__(self, x, y, width, height, speed)
 		if window is None:
 			print("Error: no window to draw block-id #{}".format(self.id))
 			sys.exit(0)
@@ -176,9 +170,9 @@ class Drawblock(Block):
 	"""draw the block on the screen"""
 	def draw(self, form="rect"):
 		if form is "rect":
-			pygame.draw.rect(self.window, self.color, (self.xbegin, self.ybegin, self.larg, self.haut), self.fill)
+			pygame.draw.rect(self.window, self.color, (self.xbegin, self.ybegin, self.width, self.height), self.fill)
 		elif form is "circle":
-			pygame.draw.circle(self.window, self.color, (int(self.xbegin), int(self.ybegin)), self.larg//2, self.fill)
+			pygame.draw.circle(self.window, self.color, (int(self.xbegin), int(self.ybegin)), self.width//2, self.fill)
 
 	def __repr__(self):
 		string = Block.__repr__(self)
@@ -193,8 +187,8 @@ class Picblock(Block):
 	namepic = name of the picture
 	pic = picture load
 	"""
-	def __init__(self, x=0, y=0, larg=100, haut=100, speed=0, namepic=None, window=None):
-		Block.__init__(self, x, y, larg, haut, speed)
+	def __init__(self, x=0, y=0, width=100, height=100, speed=0, namepic=None, window=None):
+		Block.__init__(self, x, y, width, height, speed)
 		if window is None:
 			print("Error: no window to print block-id #{}".format(self.id))
 			sys.exit(0)
@@ -204,19 +198,19 @@ class Picblock(Block):
 		self.window = window
 		self.namepic = namepic
 		self.pic = pygame.image.load(self.namepic).convert_alpha()
-		self.pic = pygame.transform.scale(self.pic, (self.larg, self.haut))
+		self.pic = pygame.transform.scale(self.pic, (self.width, self.height))
 	"""print the picture block on the screen"""
 	def print(self):
 		self.window.blit(self.pic, (self.xbegin, self.ybegin))
 	"""set the picture with an other pic and other dimensions"""
-	def set_pic(self, pic, larg=None, haut=None):
-		if larg is not None:
-			self.larg = larg
-		if haut is not None:
-			self.haut = haut
+	def set_pic(self, pic, width=None, height=None):
+		if width is not None:
+			self.width = width
+		if height is not None:
+			self.height = height
 		self.namepic = pic
 		self.pic = pygame.image.load(self.namepic).convert_alpha()
-		self.pic = pygame.transform.scale(self.pic, (self.larg, self.haut))
+		self.pic = pygame.transform.scale(self.pic, (self.width, self.height))
 		self.set_position(self.xbegin, self.ybegin)
 
 	def __repr__(self):
--- src/window.py
+++ src/window.py
@@ -7,15 +7,15 @@ class Window:
 	"""
 	Window -> create a basic pygame window
 	win: the pygame window -> pygame.Surface()
-	wlarg = width of the window
-	whaut = height of the window
+	wwidth = width of the window
+	wheight = height of the window
 	wtitle = title of the window
 	"""
-	def __init__(self, wlarg=500, whaut=500, wtitle="unknown"):
-		self.wlarg = wlarg
-		self.whaut = whaut
+	def __init__(self, wwidth=500, wheight=500, wtitle="unknown"):
+		self.wwidth = wwidth
+		self.wheight = wheight
 		self.wtitle = wtitle
-		self.win = pygame.display.set_mode((wlarg, whaut))
+		self.win = pygame.display.set_mode((wwidth, wheight))
 		pygame.display.set_caption(wtitle)
 
 	"""same role as Surface.fill()"""
@@ -25,10 +25,10 @@ class Window:
 	def get_canva(self):
 		return self.win
 	"""set the size of the window"""
-	def set_size(self, wlarg=500, whaut=500):
-		self.wlarg = wlarg
-		self.whaut = whaut
-		self.win = pygame.display.set_mode((wlarg, whaut))
+	def set_size(self, wwidth=500, wheight=500):
+		self.wwidth = wwidth
+		self.wheight = wheight
+		self.win = pygame.display.set_mode((wwidth, wheight))
 	"""set the title of the window"""
 	def set_title(self, string):
 		pygame.display.set_caption(string)
