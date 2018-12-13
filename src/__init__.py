1b408567b20ff1ab6c15abb1f815f929935cce4f
DaveTilheim <mb624967@skynet.be>
2018-12-13 23:33:52 +0100

    del unusual feature

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,3 @@ import webbrowser as wb
 import os
 name = "izi_pygame"
 doc = lambda:wb.open("https://github.com/DaveTilheim/izi-pygame/tree/master/doc")
-dopen = lambda:os.system("open -a SublimeText /Users/ArthurDetrembleur/Desktop/gitHubProject/izi-pygame/doc")
-popen = lambda:os.system("open -a SublimeText /Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/izi_pygame/*.py")
-eg1 = lambda:os.system("python3.7 /Users/ArthurDetrembleur/Desktop/prog_perso/cercle_tmul/main.py")
